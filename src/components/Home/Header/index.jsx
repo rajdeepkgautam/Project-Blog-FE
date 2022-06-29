fca18345a95ecc5ee5854ab4f0139f07351b7804
RAJDEEP KUMAR GAUTAM <rajdeepkgautam@gmail.com>
2022-06-29 18:38:00 +0530

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='page-header'>
+  <h2> The Knowledge Hunt</h2>
+        <h1>
+            <span>-</span> Blog <span>-</span>
+        </h1>
+        <p>
+            Your Daily Dose of <br/> Awesome  & Productive Updates.
+        </p>
+  </header>
+);
+
+export default Header;
